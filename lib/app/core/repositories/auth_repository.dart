1e4bbac031c0d720a6c5fdd36a424c61b00c37e1
codewithmujab <saefulmujab300@gmail.com>
2024-06-11 11:28:49 +0700

    initial commit

---
+++ lib/app/core/repositories/auth_repository.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_bloc_boilerplate/app/core/config/api.service.dart';
+import 'package:flutter_bloc_boilerplate/app/core/config/api_endpoints.dart';
+import 'package:flutter_bloc_boilerplate/app/services/shared_pref.dart';
+
+class AuthRepository {
+  static late String userToken;
+  static late int userId;
+
+  final HttpClient _httpClient = HttpClient();
+  final SharedPreferencesManager _sharedPreferencesManager =
+      SharedPreferencesManager();
+
+  Future<void> init() async {
+    try {
+      final token = await _sharedPreferencesManager.getString('_authToken');
+      final uid = await _sharedPreferencesManager.getInt('user_id');
+      userId = uid!;
+      userToken = '$token';
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  //login
+  Future<HttpResponse> loginWithEmailPassword(dynamic param) async {
+    HttpResponse response = await _httpClient.postPublic(
+        ApiEndPoints.loginWithEmailPassword, param);
+    return response;
+  }
+
+  //create account
+  Future<HttpResponse> createAccount(dynamic param) async {
+    HttpResponse response =
+        await _httpClient.postPublic(ApiEndPoints.registerAccount, param);
+    return response;
+  }
+
+  //save token
+  Future<void> saveToken(String token, int uid) async {
+    _sharedPreferencesManager.putString('_authToken', token);
+    _sharedPreferencesManager.putBool('isLoggedIn', true);
+    _sharedPreferencesManager.putInt('user_id', uid);
+  }
+
+  //logout
+  Future<HttpResponse> logout() async {
+    HttpResponse response =
+        await _httpClient.postPrivate(ApiEndPoints.logout, {}, userToken);
+    return response;
+  }
+}
---
+++ lib/app/env.dart
@@ -0,0 +1,4 @@
+class Environments {
+  static const String appName = 'Flutter Bloc Boilerplate';
+  static const String apiBaseURL = 'http://110.138.90.209:8002/';
+}
